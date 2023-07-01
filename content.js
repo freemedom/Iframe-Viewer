78d4e3274be8b1c3a0d98e58b785cebe6d60a6d7
freemedom <57294686+freemedom@users.noreply.github.com>
2023-07-01 21:04:50 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,29 +1,32 @@
 // console.log(1)
 // debugger
-if (self == top) {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        switch (request.from) {
-            case "popup":
-                var iframes = Array.prototype.slice.call(document.getElementsByTagName("iframe"));
-                var frames = Array.prototype.slice.call(document.getElementsByTagName("frame"));
-                iframes = iframes.concat(frames)
-    
-                const iframeUrls = iframes.map(iframe => iframe?.src)
-                    .filter(url => url != null && url != "about:blank");
-                sendResponse({ iframeUrls: iframeUrls });
-                break;
-            case "background":
-                var numIframes = document.getElementsByTagName("iframe").length + document.getElementsByTagName("frame").length;
-                sendResponse({ numIframes: numIframes, tabId: request["tabId"] });
-        }
-    });
-}
-else {
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    switch (request.from) {
+        case "popup":
+            var iframes = Array.prototype.slice.call(document.getElementsByTagName("iframe"));
+            var frames = Array.prototype.slice.call(document.getElementsByTagName("frame"));
+            iframes = iframes.concat(frames)
+
+            const iframeUrls = iframes.map(iframe => iframe?.src)
+                .filter(url => url != null && url != "about:blank");
+            sendResponse({ iframeUrls: iframeUrls });
+            if (self != top) {
+                update_storage();
+            }
+            break;
+        case "background":
+            var numIframes = document.getElementsByTagName("iframe").length + document.getElementsByTagName("frame").length;
+            sendResponse({ numIframes: numIframes, tabId: request["tabId"] });
+    }
+});
+
+function update_storage() {
     var map1;
     // try {
-        
+
     // } catch (error) {
-        
+
     // }
 
     chrome.storage.local.get(["html_storage"]).then((result) => {
@@ -31,12 +34,13 @@ else {
         // console.log("Value currently is " + result.html_storage);
         if (result.html_storage != undefined) {
             map1 = new Map(JSON.parse(result.html_storage));
-        }
-        else{
+        } else {
             map1 = new Map();
         }
         map1.set(location.href, document.documentElement.outerHTML);
-        chrome.storage.local.set({ "html_storage": JSON.stringify(Array.from(map1.entries()))})
+        chrome.storage.local.set({
+            "html_storage": JSON.stringify(Array.from(map1.entries()))
+        })
     });
     //then里面的函数是异步执行，慢
     console.log(1)
